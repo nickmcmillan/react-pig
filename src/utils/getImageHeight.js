c9b136075e7ea1c32026805573d8196bcf1b5910
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-02-21 17:35:24 +0100

    💄 improve img quality

--- src/utils/getImageHeight.js
+++ src/utils/getImageHeight.js
@@ -1,5 +1,5 @@
 export default function(containerWidth) {
-  if (containerWidth <= 640) return 400
-  if (containerWidth <= 1920) return 500
-  return 600
+  if (containerWidth <= 640) return 600
+  if (containerWidth <= 1920) return 800
+  return 1000
 }
