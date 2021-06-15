b913ca67922daf598635ac15675fcc2a07929e84
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-06-15 15:16:15 +0200

    Add example on how to implement selectable images in a parent component

--- example/src/index.js
+++ example/src/index.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Pig from 'pig-react'
+import SelectablePig from './SelectablePig'
 import imageData from './imageData.json'
 
 import './base.css'
 
 ReactDOM.render(
   <main className="main">
+    {
+      // <SelectablePig/>
+    }
     <Pig
       imageData={imageData}
       gridGap={8}
       bgColor="hsla(211, 50%, 98%)"
-
       groupGapLg={50}
       groupGapSm={20}
+      selectable={true}
       breakpoint={800}
       // sortByDate
-      groupByDate
+      // groupByDate
     />
   </main>
 , document.getElementById('root'))
